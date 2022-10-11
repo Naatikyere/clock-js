2d68089123505f61a0c59891f8414aaa3a97c0b1
prosperking <naatikyereprosper@gmail.com>
2022-10-11 21:31:09 +0100

    Done but would try to set different functions fot all hands nxt time

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 //call the hand properties
 const secondHand = document.querySelector(".sec_hand");
-const min_hand = document.querySelector(".min-hand");
+const min_hand = document.querySelector(".min_hand");
+const hour_hand = document.querySelector(".hour_hand");
 function setsecs(){
     const now = new Date();
     const seconds = now.getSeconds();
     // need to convert the seconds to degree
-    const secondDegree = ((seconds/60)*360) + 90;//needs to let it start from the top so add 90
+    const secondDegree = ((seconds/60)*360) +90;//needs to let it start from the top so add 90
    /* apply syle to the seconds hand so
    that it changes with the changes in seconds values
    */
   secondHand.style.transform = `rotate(${secondDegree}deg)`; 
-  console.log(seconds);   
-}
-function setmin(){
-    const now = new Date();
-    const minites = now.setMinutes();
+  console.log(seconds);  
+    const minites = now.getMinutes();
+    const minDegree = ((minites/60 * 360) )+ 80;
+    min_hand.style.transform= `rotate(${minDegree}deg)`;
     console.log(minites);
+    const hour = now.getHours();
+    const hourDegree = ((hour / 60 * 360) )+ 90;
+    min_hand.style.transform= `rotate(${hourDegree}deg)`;
+    console.log(hour);
+     
 }
 setInterval(setsecs, 1000);
-setInterval(setmin,6000);
